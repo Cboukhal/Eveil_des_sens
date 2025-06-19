eca90408385ecaff1187a4b87656ae6be2d5e41a
Cboukhal <boukhalfa.camil@hotmail.fr>
2025-06-19 16:15:28 +0200

    eval

--- asset/Js/script.js
+++ asset/Js/script.js
@@ -81,33 +81,84 @@ for(let champ of champs){
     })
 }
 
-const form = document.querySelector('.contact form');
-const reussi = document.querySelector('.reussite');
-form.addEventListener('submit',(element)=>{
-        element.preventDefault();
-        reussi.style.display = "block";
-        setTimeout(()=>{
-            reussi.style.display = "none";
-        },5000)
-    })
+// const form = document.querySelector('.contact form');
+// const reussi = document.querySelector('.reussite');
+// form.addEventListener('submit',(element)=>{
+//         element.preventDefault();
+//         reussi.style.display = "block";
+//         setTimeout(()=>{
+//             reussi.style.display = "none";
+//         },5000)
+//     })
 
 /*-------------------------------------Reservation--------------------------------------*/
-const formr = document.getElementById('form-reservation');
-        const popupr = document.getElementById('popup');
+// const formr = document.getElementById('form-reservation');
+//         const popupr = document.getElementById('popup');
 
-        formr.addEventListener('submit', function(event) {
-            event.preventDefault();
-            popupr.style.display = 'block';
-            setTimeout(() => {
-                popupr.style.display = 'none';
-                formr.reset();
-            }, 4000);
-        });
+//         formr.addEventListener('submit', function(event) {
+//             event.preventDefault();
+//             popupr.style.display = 'block';
+//             setTimeout(() => {
+//                 popupr.style.display = 'none';
+//                 formr.reset();
+//             }, 4000);
+//         });
 
 /*-------------------------Estimation-------------------------*/
 let menu_s = document.getElementById('menu_s');
 let menu_c = document.getElementById('menu_c');
 let menu_b = document.getElementById('menu_b');
 let menu_d = document.getElementById('menu_d');
+let saveurs = document.getElementById('menus_saveurs');
+let carte = document.getElementById('menu_carte');
+let boissons = document.getElementById('boissons');
+let dessert = document.getElementById('dessert');
+const HT = document.getElementById('HT');
 
-console.log(menu_s);
+menu_s.addEventListener('click', () => {
+    const Menu = saveurs.value;
+    const texteMenu = saveurs.options[saveurs.selectedIndex].text;
+
+    total_m.innerHTML += "<li>";
+    total_m.innerHTML += texteMenu;
+    total_m.innerHTML += "</li>";
+    HT.innerHTML += "<li>";
+    HT.innerHTML += Menu;
+    HT.innerHTML += "</li>";
+
+});
+menu_c.addEventListener('click', () => {
+    const Menu = carte.value;
+    const texteMenu = carte.options[carte.selectedIndex].text;
+
+    total_c.innerHTML += "<li>";
+    total_c.innerHTML += texteMenu;
+    total_c.innerHTML += "</li>";
+    HT.innerHTML += "<li>";
+    HT.innerHTML += Menu;
+    HT.innerHTML += "</li>";
+
+});
+menu_b.addEventListener('click', () => {
+    const Menu = boissons.value;
+    const texteMenu = boissons.options[boissons.selectedIndex].text;
+
+    total_b.innerHTML += "<li>";
+    total_b.innerHTML += texteMenu;
+    total_b.innerHTML += "</li>";
+    HT.innerHTML += "<li>";
+    HT.innerHTML += Menu;
+    HT.innerHTML += "</li>";
+
+});
+menu_d.addEventListener('click', () => {
+    const Menu = dessert.value;
+    const texteMenu = dessert.options[dessert.selectedIndex].text;
+
+    total_d.innerHTML += "<li>";
+    total_d.innerHTML += texteMenu;
+    total_d.innerHTML += "</li>";
+    HT.innerHTML += "<li>";
+    HT.innerHTML += Menu;
+    HT.innerHTML += "</li>";
+});
